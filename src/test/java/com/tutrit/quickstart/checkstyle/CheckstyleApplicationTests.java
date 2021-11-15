b7a002ae668ce0d929c33416fa2d328f3a3bc9bb
max-java <6666350@gmail.com>
2021-11-15 11:01:40 +0100

    adds checkstyle.xml configuration and org.springframework restriction

---
+++ src/test/java/com/tutrit/quickstart/checkstyle/CheckstyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutrit.quickstart.checkstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
